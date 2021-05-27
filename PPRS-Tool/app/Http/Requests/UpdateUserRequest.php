319063f4dbdbeb747c908b0143d8a2a7fd9ad380
daniel t <34688156+dtek210@users.noreply.github.com>
2021-05-26 18:40:29 -0700

    fixed user edit bugs
    now can edit users without error 403

--- PPRS-Tool/app/Http/Requests/UpdateUserRequest.php
+++ PPRS-Tool/app/Http/Requests/UpdateUserRequest.php
@@ -3,28 +3,33 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 
 class UpdateUserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-            //
+            'name'    => [
+                'string',
+                'required',
+            ],
+            'email'   => [
+                'required',
+                'unique:users,email,' . request()->route('user')->id,
+            ],
+            'roles.*' => [
+                'integer',
+            ],
+            'roles'   => [
+                'required',
+                'array',
+            ],
         ];
+    }
+
+    public function authorize()
+    {
+        return Gate::allows('user_access');
     }
 }
